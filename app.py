ea25780d4c2a82b6b6257dd3c348a089288904a4
sekimori0802 <t.s.0514.0952@gmail.com>
2025-01-31 13:59:25 +0900

    🔒 feat: HTTPSサポートを追加
    - pyOpenSSLパッケージを追加
    - Flaskアプリケーションでhttpsを有効化

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def health_check():
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 4000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True, ssl_context='adhoc')
